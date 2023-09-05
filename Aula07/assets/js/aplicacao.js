93d23467035291ca78ac9e06f30872b17d310a8c
Thiago August <wstagf@gmail.com>
2023-09-05 20:42:37 -0300

    Aplicacao adicionando dentro da sessao

---
+++ Aula07/assets/js/aplicacao.js
@@ -0,0 +1,74 @@
+const produtos = [
+    {
+        tipo: "capacete",
+        nome: "Capacete 1",
+        alt: "Isto é um capacete, modelo 1",
+        preco: 50,
+        imagemURL: 'assets/images/capacete01.png'
+    },
+    {
+        tipo: "capacete",
+        nome: "Capacete 2",
+        alt: "Isto é um capacete, modelo 2",
+        preco: 25,
+        imagemURL: 'assets/images/capacete02.png'
+    },
+    {
+        tipo: "capacete",
+        nome: "Capacete 3",
+        alt: "Isto é um capacete, modelo 3",
+        preco: 70,
+        imagemURL: 'assets/images/capacete03.png'
+    },
+    {
+        tipo: "blusa",
+        nome: "Blusão 1",
+        alt: "Isto é um Blusão, modelo 1",
+        preco: 50,
+        imagemURL: 'assets/images/blusa01.png'
+    },
+    {
+        tipo: "blusa",
+        nome: "Blusão 2",
+        alt: "Isto é um Blusão, modelo 2",
+        preco: 25,
+        imagemURL: 'assets/images/blusa02.png'
+    },
+    {
+        tipo: "blusa",
+        nome: "Blusão 3",
+        alt: "Isto é um Blusão, modelo 3",
+        preco: 70,
+        imagemURL: 'assets/images/blusa03.png'
+    }
+
+]
+
+const criaProduto = (nome, alt, preco, imagemURL, elemento) => {
+
+    elemento.innerHTML = elemento.innerHTML 
+    +  "<div class='produto'>"
+    + " <img src='"+imagemURL+"' alt='"+alt+"'/>"
+    + "<h2>"+ nome + "</h2>"
+    + "<p>R$ " + preco+ "</p>"
+    + "<button class='adicionarCarrinho'> Adicionar ao Carrinho</button>"
+    + "</div>";
+}
+
+const buscarProdutos = () => {
+
+    // é aqui que iriamos no banco de dados ou  na api.. buscar os produtos... 
+    const lista = produtos;
+    lista.forEach((item) => {
+        let elemento = ""
+        if(item.tipo === "capacete" ) {
+            elemento  = document.getElementById('capacetes');
+        } else {
+            elemento  = document.getElementById('blusas');
+        }
+        
+        criaProduto(item.nome, item.alt,  item.preco, item.imagemURL, elemento);
+    })
+
+
+}
