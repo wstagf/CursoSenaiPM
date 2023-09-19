615173e866ae9c63dd1a997f2531e45e6532a460
Thiago August <wstagf@gmail.com>
2023-09-18 21:40:26 -0300

    Implementado o metodo delete

---
+++ Aula11/acessorestrito/assets/js/crud.js
@@ -0,0 +1,117 @@
+let produtos = [];
+
+
+const buscarProdutos = async () => {
+    limparTela();
+    produtos = [];
+    console.log('buscarProdutos')
+    var jwt = window.localStorage.getItem("jwt");
+    fetch('http://localhost:1337/api/produtos', {
+        method: "GET",
+        headers: {
+                "Content-type": "application/json; charset=UTF-8",
+                "Authorization": "bearer " + jwt
+            }
+        })
+        .then((resp1) => {
+            if(resp1.status == 401) {
+                window.location.href = "../sem-acesso.html";
+            } else {
+                return resp1.json();
+            }  
+        })
+        .then(  (resp2) => { 
+
+            resp2.data.forEach((itemDaAPI, index) => {
+                let elemento = ""
+
+                const cadaItemConvertido = {
+                    "id": itemDaAPI.id,
+                    "tipo": itemDaAPI.attributes.tipo,
+                    "nome" : itemDaAPI.attributes.nome,
+                    "alt" : itemDaAPI.attributes.alt,
+                    "preco" : itemDaAPI.attributes.preco,
+                    "imagemURL" : itemDaAPI.attributes.imagemURL,
+                    "preco" : itemDaAPI.attributes.preco
+                }
+
+                 
+                produtos.push(cadaItemConvertido); 
+                
+                criaProduto(cadaItemConvertido.nome, cadaItemConvertido.id,  
+                    cadaItemConvertido.preco, cadaItemConvertido.imagemURL, 
+                    cadaItemConvertido.tipo,  index);
+            })
+        })
+        .catch((erro) => {
+            console.log("deu erro", erro)
+        })
+}
+const limparTela = () =>  {
+    const elemento =  document.getElementById('criarprodutos');
+    elemento.innerHTML = "";
+}
+
+const criaProduto = (nome, id, preco, imagemURL, tipo,  index) => {
+    console.log("p")
+     const elemento = document.getElementById('criarprodutos');
+      
+
+    const item = document.createElement('tr')
+    item.innerHTML = 
+    "<td>"+ id + "</td>" + 
+    "<td>" + tipo+ "</td>" +  
+    "<td>" +nome+ "</td>" + 
+    "<td>" + preco + "</td>" +  
+    
+    "<td><img src='" + imagemURL + "' style='width:50px;'></img></td>" + 
+    "<td>"+ 
+        "<a href='#'>"+
+          "<i class='fa fa-trash' aria-hidden='true' onclick='excluirItem("+id +")'></i>"+
+         "</a>"+
+      "</td>" ;
+    elemento.appendChild(item);
+
+         
+
+
+    // elemento.innerHTML = elemento.innerHTML 
+    // +  "<div class='produto'>"
+    // + " <img src='"+imagemURL+"' alt='"+alt+"'/>"
+    // + "<h2>"+ nome + "</h2>"
+    // + "<p>R$ " + preco+ "</p>"
+    // + "<button class='adicionarCarrinho' "
+    //    + "onclick='adicionarCarrinho("+ index +")'> "
+    // + "Adicionar ao Carrinho</button>"
+    // + "</div>";
+}
+
+
+const excluirItem = (id) => {
+    console.log(id)
+
+    var jwt = window.localStorage.getItem("jwt");
+    fetch('http://localhost:1337/api/produtos/' + id, {
+        method: "DELETE",
+        headers: {
+                "Content-type": "application/json; charset=UTF-8",
+                "Authorization": "bearer " + jwt
+            }
+        })
+        .then((resp1) => {
+            if(resp1.status == 401) {
+                window.location.href = "../sem-acesso.html";
+            } else {
+                return resp1.json();
+            }  
+        })
+        .then(  (resp2) => { 
+            alert('Excluido com sucesso');
+            buscarProdutos();
+        })
+        .catch((erro) => {
+            console.log("deu erro", erro)
+        })
+}
+
+buscarProdutos();
--- Aula11/roteiro.md
+++ Aula11/roteiro.md
@@ -19,3 +19,54 @@ OpenAPI Specification
 - realtime websocket
 https://medium.com/@gabrielfgularte/understanding-realtime-web-apps-5aef47d51a45
 
+
+
+
+
+ fetch('http://localhost:1337/api/produtos', {
+        method: "GET",
+        headers: {
+                "Content-type": "application/json; charset=UTF-8",
+                "Authorization": "bearer " + jwt
+            }
+        })
+        .then((resp1) => {
+            if(resp1.status == 401) {
+                window.location.href = "../sem-acesso.html";
+            } else {
+                return resp1.json();
+            }  
+        })
+        .then(  (resp2) => { 
+            console.log(resp2)
+             
+        })
+        .catch((erro) => {
+            console.log("deu erro", erro)
+        })
+
+
+
+# Metodo C.R.U.D. = READ ALL .... buscarProdutos()
+
+quando a tela é criada, 
+1. o javascript faz a busca na rede, 
+2. o javascript adiciona o item no array
+3. o java script adiciona cada item em um linha da tabela, personalizando o onclick
+  -  "<td>"+ 
+        "<a href='#'>"+
+          "<i class='fa fa-trash' aria-hidden='true' onclick='excluirItem("+id +")'></i>"+
+         "</a>"+
+      "</td>" ; 
+
+
+
+
+# Metodo C.R.U.D. = DELETE  .... excluirItem(1)
+1. O java script chama a api com o methodo DELETE e o ID na url 
+
+fetch('http://localhost:1337/api/produtos/' + id, {
+        method: "DELETE",
+
+2. Se der certo a requisição, o javascript limpa a tela
+3. o java scrpit recarrega os item (Chama o metodo buscarProdutos )
