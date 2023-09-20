cb0e7b954b5dabf92e8c6af963f0462c980ce6db
Thiago August <wstagf@gmail.com>
2023-09-19 21:19:54 -0300

    algoritmos

---
+++ PHP/Aula01/algoritimo.php
@@ -0,0 +1,32 @@
+<?php 
+
+
+echo('<h1>Operadores Aritimeticos</h1>');
+echo('<h1>'. (4  + 8).' = 12</h1>');
+echo('<h1>'. (4  - 8).' = -4 </h1>');
+echo('<h1>'. (4  * 2).' = 8</h1>');
+echo('<h1>'. (4  / 2).'= 2</h1>');
+
+echo('<br/><br/><br/><br/><br/><br/>');
+
+echo('<h1>Operadores Aritimeticos</h1>');
+echo('<h1>== atribuição, igual o java script </h1>');
+echo('<h1> <> Diferente , != diferente </h1>');
+
+
+
+echo('<br/><br/><br/><br/><br/><br/>');
+
+echo('<h1>Operadores logicos</h1>');
+echo('<h1>&& --- E, || ou...., igual o java script </h1>');
+echo('<h1> and --- E , or .... ou...</h1>');
+
+
+
+echo('<br/><br/><br/><br/><br/><br/>');
+
+echo('<h1>Operadores texto</h1>');
+echo('<h2>o operador . faz a concatenação de strings</h2>');
+echo('<h2> Curso '. "PM-GO".'</h2>');
+
+?>
