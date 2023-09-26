3ae7e7184f3d634d842593ecce7833a72759eaec
Thiago August <wstagf@gmail.com>
2023-09-26 09:18:26 -0300

    Preparando a aula

--- PHP/Aula03/view/carrinho.php
+++
@@ -1,8 +0,0 @@
-<section class="carrinho none">
-    <div class="tituloCarrinho">
-        <h1>Carrinho</h1>
-        <a href="#" onclick="fecharCarrinho()">X</a>
-    </div>
-    <p>Total R$ <span id="valorTotalCarrinho">0,00</span></p>
-    <ul id="itensCarrinho"></ul>
-</section>
--- PHP/Aula03/view/produtos.php
+++
@@ -1,6 +0,0 @@
-<main > 
-    <section id="capacetes" style="border: 1px solid blue;"> <!-- capacete -->
-    </section>
-    <section id="blusas" style="border: 1px solid red;"> <!-- blusão -->
-    </section>
-</main>
