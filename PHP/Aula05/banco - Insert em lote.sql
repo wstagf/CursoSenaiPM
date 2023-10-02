3b2babb9668211324bd95cc87c5c350c54700783
Thiago Augusto <wstagf@gmail.com>
2023-10-02 20:25:25 -0300

    script para inserir produtos em lote

---
+++ PHP/Aula05/banco - Insert em lote.sql
@@ -0,0 +1,13 @@
+
+
+INSERT INTO pmgo.produtos
+(tipo, nome, alt, imagemURL, preco)
+VALUES('capacete', 'Capcacete 01', 'Isto é um capacete, modelo 1', 'assets/images/capacete01.png', 50),
+('capacete', 'Capcacete 02', 'Isto é um capacete, modelo 2', 'assets/images/capacete02.png', 25),
+('capacete', 'Capcacete 03', 'Isto é um capacete, modelo 3', 'assets/images/capacete03.png', 70),
+('blusa', 'Blusão 01', 'Isto é um Blusão, modelo 1', 'assets/images/blusa0.png', 50),
+('blusa', 'Blusão 02', 'Isto é um Blusão, modelo 2', 'assets/images/blusa02.png', 25),
+('blusa', 'Blusão 03', 'Isto é um Blusão, modelo 3', 'assets/images/blusa03.png', 70);
+
+
+
