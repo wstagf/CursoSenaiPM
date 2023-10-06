fbfc04049e36c4dc01a93de6e2667e6cbc7aa183
Thiago Augusto <wstagf@gmail.com>
2023-10-05 21:38:40 -0300

    Refatorado editar produtos

--- PHP/Aula07/api/produtos/produtos.php
+++ PHP/Aula07/api/produtos/produtos.php
@@ -9,67 +9,13 @@
       Produtos::obterPorID($item);
     }
 
-
-  if ($method=='POST') { 
-      Produtos::inserirProduto($_POST);
-  }
-
-
+    if ($method=='POST') { 
+        Produtos::inserirProduto($_POST);
+    }
   
-  if ($method=='PUT') { 
-    // obtem o que foi pasado e salva na variavel formdata
-    $form_data= json_encode(file_get_contents("php://input")); 
-      $key_size=52;
-      $key=substr($form_data, 1, $key_size);
-
-      $acc_params=explode($key,$form_data);
-      array_shift($acc_params);
-      array_pop($acc_params);
-      foreach ($acc_params as $item_hack){
-          $start_key=' name=\"';
-          $end_key='\"\r\n\r\n';
-          $start_key_pos=strpos($item_hack,$start_key)+strlen($start_key);
-          $end_key_pos=strpos($item_hack,$end_key);
-          
-          $key=substr($item_hack, $start_key_pos, ($end_key_pos-$start_key_pos));
-          
-          $end_value='\r\n';
-          $value=substr($item_hack, $end_key_pos+strlen($end_key), -strlen($end_value));
-          $_PUT[$key]=$value;
-      }
-      $GLOBALS["_PUT"]=$_PUT;
-    
-
-    $sql = "UPDATE produtos SET ";
-
-    $contador = 1;
-
-     // array_keys retorna as chaves de um array
-    foreach (array_keys($_PUT) as $indice) {
-      if (count($_PUT) > $contador) {
-        $sql .= "{$indice} = '{$_PUT[$indice]}', ";
-      } else {
-        $sql .= "{$indice} = '{$_PUT[$indice]}' ";
-      }
-      $contador++;
+    if ($method=='PUT') { 
+      Produtos::editarProduto($item);    
     }
-
-
-    $sql .= " WHERE id={$item}";
-
-    // var_dump($sql);
-    $db = DB::connect();
-    $rs = $db->prepare($sql);
-    $exec = $rs->execute();
-
-    if ($exec) {
-        echo json_encode(["dados" => 'Dados atualizados com sucesso.']);
-    } else {
-        echo json_encode(["dados" => 'Houve erro ao atualizar dados.']);
-    }
-
-    
-  }
 
 
   if ($method=='DELETE') { 
--- PHP/Aula07/controler/produtos/produtos.class.php
+++ PHP/Aula07/controler/produtos/produtos.class.php
@@ -79,4 +79,58 @@ class Produtos {
         echo json_encode(["dados" => 'Houve algum erro ao inseris os dados.']);
     }
   }
+
+
+  public static function editarProduto($id) {
+     // obtem o que foi pasado e salva na variavel formdata
+     $form_data= json_encode(file_get_contents("php://input")); 
+      $key_size=52;
+      $key=substr($form_data, 1, $key_size);
+
+      $acc_params=explode($key,$form_data);
+      array_shift($acc_params);
+      array_pop($acc_params);
+      foreach ($acc_params as $item_hack){
+          $start_key=' name=\"';
+          $end_key='\"\r\n\r\n';
+          $start_key_pos=strpos($item_hack,$start_key)+strlen($start_key);
+          $end_key_pos=strpos($item_hack,$end_key);
+          
+          $key=substr($item_hack, $start_key_pos, ($end_key_pos-$start_key_pos));
+          
+          $end_value='\r\n';
+          $value=substr($item_hack, $end_key_pos+strlen($end_key), -strlen($end_value));
+          $_PUT[$key]=$value;
+      }
+      $GLOBALS["_PUT"]=$_PUT;
+    
+
+    $sql = "UPDATE produtos SET ";
+
+    $contador = 1;
+
+     // array_keys retorna as chaves de um array
+    foreach (array_keys($_PUT) as $indice) {
+      if (count($_PUT) > $contador) {
+        $sql .= "{$indice} = '{$_PUT[$indice]}', ";
+      } else {
+        $sql .= "{$indice} = '{$_PUT[$indice]}' ";
+      }
+      $contador++;
+    }
+
+
+    $sql .= " WHERE id={$id}";
+
+    // var_dump($sql);
+    $db = DB::connect();
+    $rs = $db->prepare($sql);
+    $exec = $rs->execute();
+
+    if ($exec) {
+        echo json_encode(["dados" => 'Dados atualizados com sucesso.']);
+    } else {
+        echo json_encode(["dados" => 'Houve erro ao atualizar dados.']);
+    }
+  }
 }
