cb6dcc91869c0da2678a195b24814cb218e66dca
Thiago Augusto <wstagf@gmail.com>
2023-10-23 19:42:32 -0300

    Criado Hello Word em dart

---
+++ Flutter/Aula01/HelloWord.dart
@@ -0,0 +1,19 @@
+void main() {
+  print('Olá Senai');
+
+  int numero = 10;
+  print(numero * 2);
+
+  double numeroDecimal = 3.14;
+  print(numeroDecimal);
+
+  String curso = "Senai Web ";
+
+  curso += "PMGO"; // equivale a:  curso = curso + "PMGO"
+  print(curso);
+
+  print("Este curso é $curso");
+
+  print(
+      "É necessário as chaves '{}' quando quremos ler uma propriedade do objeto ou executar um calculo");
+}
