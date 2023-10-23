59c0118635b965521d8b58ab13cad62a17df2e7f
Thiago Augusto <wstagf@gmail.com>
2023-10-23 20:20:05 -0300

    const e var no dart

---
+++ Flutter/Aula01/variaveisEConstants.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  var nome = "Meu nome";
+
+  //const nome = "Meu nome"; // se nãao removermos a palavra const o codigo não roda;
+  String curso = "Web e Mobile Senai";
+
+  print(nome);
+
+  nome = "Isto é um teste";
+
+  print(nome);
+  print(curso);
+}
