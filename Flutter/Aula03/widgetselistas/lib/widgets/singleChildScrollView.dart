ae5abe99b9a000d223fcf0c239eb57712f8c2f60
Thiago August <wstagf@gmail.com>
2023-10-26 20:43:06 -0300

    Single Child Scroll view

---
+++ Flutter/Aula03/widgetselistas/lib/widgets/singleChildScrollView.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class SingleChildScrollViewPage extends StatelessWidget {
+  const SingleChildScrollViewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 180,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Container(
+              padding: EdgeInsets.all(8),
+              color: Colors.amber,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(
+                    Icons.abc_outlined,
+                    size: 150,
+                  ),
+                  Icon(
+                    Icons.arrow_back_ios_outlined,
+                    size: 150,
+                  ),
+                  Icon(
+                    Icons.arrow_back_outlined,
+                    size: 150,
+                  ),
+                  Icon(
+                    Icons.abc_outlined,
+                    size: 150,
+                  ),
+                  Icon(
+                    Icons.arrow_back_ios_outlined,
+                    size: 150,
+                  ),
+                  Icon(
+                    Icons.arrow_back_outlined,
+                    size: 150,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+        Container(
+          height: 180,
+          child: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.all(8),
+              color: Colors.amber,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(
+                    Icons.abc_outlined,
+                    size: 150,
+                  ),
+                  Icon(
+                    Icons.arrow_back_ios_outlined,
+                    size: 150,
+                  ),
+                  Icon(
+                    Icons.arrow_back_outlined,
+                    size: 150,
+                  ),
+                  Icon(
+                    Icons.abc_outlined,
+                    size: 150,
+                  ),
+                  Icon(
+                    Icons.arrow_back_ios_outlined,
+                    size: 150,
+                  ),
+                  Icon(
+                    Icons.arrow_back_outlined,
+                    size: 150,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
