0ed6d1215dc6a4b47b93a2ede40f5f725b3ab6cf
Thiago August <wstagf@gmail.com>
2023-10-26 20:43:25 -0300

    Anotações

--- Flutter/Aula03/widgetselistas/lib/nossoMaterialApp.dart
+++ Flutter/Aula03/widgetselistas/lib/nossoMaterialApp.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:widgetselistas/widgets/sizedbox.dart';
 
 import 'homePage.dart';
+import 'widgets/aspectRatio.dart';
 import 'widgets/center.dart';
 import 'widgets/column.dart';
 import 'widgets/container.dart';
+import 'widgets/expanded.dart';
 import 'widgets/padding.dart';
 import 'widgets/row.dart';
+import 'widgets/safeArea.dart';
 import 'widgets/scaffold.dart';
+import 'widgets/singleChildScrollView.dart';
 import 'widgets/stack.dart';
 
 class NossoMaterialApp extends StatelessWidget {
@@ -18,7 +22,7 @@ class NossoMaterialApp extends StatelessWidget {
     return const MaterialApp(
       title: "Aula 03",
       home: Material(
-        child: CenterPage(),
+        child: ExpendedPage(),
       ),
     );
   }
