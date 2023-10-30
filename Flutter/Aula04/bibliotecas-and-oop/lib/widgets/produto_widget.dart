58dfb7995b6180455c23a9fe5d5def54354339ce
Thiago August <wstagf@gmail.com>
2023-10-30 20:59:51 -0300

    Criado tela inicial de produtos - AINDA ESTATICO

--- Flutter/Aula04/bibliotecas-and-oop/lib/widgets/produto_widget.dart
+++ Flutter/Aula04/bibliotecas-and-oop/lib/widgets/produto_widget.dart
@@ -5,20 +5,57 @@ class ProdutoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      height: 100,
-      child: Row(children: [
-        Container(
-          width: MediaQuery.of(context).size.width * 0.3,
-          color: Colors.red,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        color: Colors.white,
+        padding: const EdgeInsets.all(8.0),
+        height: 100,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.3,
+              child: Image.network(
+                  'https://logodownload.org/wp-content/uploads/2019/08/senai-logo-1.png'),
+            ),
+            Expanded(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.3,
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Nome do produto",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      "Descrição sussinta do produto",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 12,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Valor do produto",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
-        Expanded(
-            child: Container(
-          width: MediaQuery.of(context).size.width * 0.3,
-          color: Colors.blue,
-        ))
-      ]),
+      ),
     );
   }
 }
