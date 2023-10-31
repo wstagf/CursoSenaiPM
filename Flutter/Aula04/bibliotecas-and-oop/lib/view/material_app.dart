85574483cea59b7aa54ffce5e3b539b2fe7f53ed
Thiago August <wstagf@gmail.com>
2023-10-30 21:55:25 -0300

    Cor do tema da aplicacao

--- Flutter/Aula04/bibliotecas-and-oop/lib/view/material_app.dart
+++ Flutter/Aula04/bibliotecas-and-oop/lib/view/material_app.dart
@@ -7,9 +7,14 @@ class NossaMaterialApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashPage(),
+      home: const SplashPage(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.green,
+        ),
+      ),
     );
   }
 }
