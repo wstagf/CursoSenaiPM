8a79e5ebdd9fbcbf3693303d5397d8abab6f1ccc
Thiago August <wstagf@gmail.com>
2023-10-31 21:52:23 -0300

    colocado a responsabilidade de ir na api dentro do arquivo .service...

--- Flutter/Aula05/consumindoapi/lib/controller/produto_repository.dart
+++ Flutter/Aula05/consumindoapi/lib/controller/produto_repository.dart
@@ -2,47 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 import '../model/produto_model.dart';
+import 'produto_service.dart';
 
 class ProdutoRepository {
   List<ProdutoModel> produtos = [];
+  ProdutoService service = ProdutoService();
 
   Future<void> buscarProdutos() async {
     // ... vai na api..
 
-    await Future.delayed(const Duration(seconds: 3));
+    await Future.delayed(const Duration(seconds: 0));
 
-    var sucesso = true;
+    List<ProdutoModel> listaRetorno = await service.buscaProdutosNaAPI();
 
-    if (sucesso == true) {
-      produtos = [
-        ProdutoModel(
-          id: 1,
-          nome: "Produto 001",
-          alt: "Texto Alternativo do produto 001",
-          imagemURL:
-              "https://urbanhelmets.fbitsstatic.net/img/p/capacete-urban-bigbore-double-d-green-flake-vintage-110639/298272-9.jpg?w=1000&h=1000&v=no-change&qs=ignore",
-          preco: 199.50,
-          tipo: "Capacete",
-        ),
-        ProdutoModel(
-          id: 2,
-          nome: "Produto 002",
-          alt: "Texto Alternativo do produto 002",
-          imagemURL:
-              "https://urbanhelmets.fbitsstatic.net/img/p/capacete-urban-desperado-double-d-live-green-110685/298339-1.jpg?w=1000&h=1000&v=no-change&qs=ignore",
-          preco: 299.50,
-          tipo: "Capacete",
-        ),
-        ProdutoModel(
-          id: 3,
-          nome: "Produto 003",
-          alt: "Texto Alternativo do produto 003",
-          imagemURL:
-              "https://urbanhelmets.fbitsstatic.net/img/p/capacete-urban-sports-laranja-110919/299112-1.jpg?w=1000&h=1000&v=no-change&qs=ignore",
-          preco: 399.50,
-          tipo: "Capacete",
-        ),
-      ];
+    if (listaRetorno.isNotEmpty) {
+      produtos = listaRetorno;
     } else {
       Fluttertoast.showToast(
         msg: "Houve um erro ao carregar os dados",
@@ -61,7 +35,7 @@ class ProdutoRepository {
 
     produtos = [];
 
-    await Future.delayed(const Duration(seconds: 1));
+    await Future.delayed(const Duration(seconds: 0));
 
     await buscarProdutos();
   }
