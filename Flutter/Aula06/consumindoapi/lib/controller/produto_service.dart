69652486c8c6699c3fce1445240138017a0b1466
Thiago Augusto <wstagf@gmail.com>
2023-11-07 23:25:21 -0300

    Alteracao do endpoint

--- Flutter/Aula06/consumindoapi/lib/controller/produto_service.dart
+++ Flutter/Aula06/consumindoapi/lib/controller/produto_service.dart
@@ -7,8 +7,7 @@ class ProdutoService {
   Future<List<ProdutoModel>> buscaProdutosNaAPI() async {
     List<ProdutoModel> lista = [];
     try {
-      var url = Uri.parse(
-          'https://run.mocky.io/v3/68cfd34d-3fb3-41c6-9725-39dba34c667d');
+      var url = Uri.parse('http://192.168.15.5:1337/api/produtos/');
       var response = await http.get(url);
 
       lista = (json.decode(response.body) as List)
