2621bf47eef671f79f7356222fb2c1cc66149688
Thiago Augusto <wstagf@gmail.com>
2023-11-07 23:49:48 -0300

    clicando no item de produto e mostrando o id deste item

--- Flutter/Aula06/consumindoapi/lib/view/widgets/produto_widget.dart
+++ Flutter/Aula06/consumindoapi/lib/view/widgets/produto_widget.dart
@@ -12,47 +12,79 @@ class ProdutoWidget extends StatelessWidget {
       child: Container(
         color: Colors.white,
         padding: const EdgeInsets.all(8.0),
-        height: 100,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
+        child: Column(
           children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.3,
-              child: Image.asset(produto.imagemURL),
-            ),
-            Expanded(
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.3,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      produto.nome,
-                      style: const TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
-                      ),
-                    ),
-                    Text(
-                      produto.alt,
-                      style: const TextStyle(
-                        color: Colors.black,
-                        fontSize: 12,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      "R\$ ${produto.preco.toStringAsFixed(2).replaceAll(".", ",")}",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 12,
-                      ),
-                    ),
-                  ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.3,
+                  child: Image.asset(produto.imagemURL),
                 ),
-              ),
+                Expanded(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "R\$ ${produto.preco.toStringAsFixed(2).replaceAll(".", ",")}",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 24,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          produto.nome,
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Text(
+                          produto.alt,
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    print('Clicou ' + produto.id.toString());
+                  },
+                  child: Container(
+                    color: Colors.green[700],
+                    height: 50,
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    child: const Center(
+                      child: Text(
+                        'COMPRAR',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          letterSpacing: 1.5,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
             )
           ],
         ),
