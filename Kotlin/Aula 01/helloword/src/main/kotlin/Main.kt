04c377a8d0dce7becafede1c834e3059b0388d79
Thiago Augusto <wstagf@gmail.com>
2023-11-17 14:37:53 -0300

    criado primeiro projeto multi plataforma

---
+++ Kotlin/Aula 01/helloword/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    println("Digite a string: ")
+    var inputString = readLine()
+    println("A entrada no formato de string é: $inputString")
+
+    println("Digite o número: ")
+    var inputNumber = Integer.valueOf(readLine())
+    println("A entrada no formato de número inteiro é: $inputNumber")
+}
+
+
+
