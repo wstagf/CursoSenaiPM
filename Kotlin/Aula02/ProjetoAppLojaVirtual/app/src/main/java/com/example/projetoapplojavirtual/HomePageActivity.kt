d82baba4469287917e9db4b90a67754502395492
Thiago Augusto <wstagf@gmail.com>
2023-11-20 21:11:34 -0300

    Rodando a splash em Kotlin

---
+++ Kotlin/Aula02/ProjetoAppLojaVirtual/app/src/main/java/com/example/projetoapplojavirtual/HomePageActivity.kt
@@ -0,0 +1,25 @@
+package com.example.projetoapplojavirtual
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projetoapplojavirtual.databinding.ActivityHomePageBinding
+
+class HomePageActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomePageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHomePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        binding.toolbarLayout.title = title
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
---
+++ Kotlin/Aula02/ProjetoAppLojaVirtual/app/src/main/java/com/example/projetoapplojavirtual/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.projetoapplojavirtual
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
