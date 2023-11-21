4374fe7499abfdb7d9708443bb567e103f2d9983
Thiago Augusto <wstagf@gmail.com>
2023-11-21 19:42:43 -0300

    nossa loja

--- Kotlin/Aula03/NossaLoja/app/src/main/java/com/example/nossaloja/MainActivity.kt
+++ Kotlin/Aula03/NossaLoja/app/src/main/java/com/example/nossaloja/MainActivity.kt
@@ -3,17 +3,19 @@ package com.example.nossaloja
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        // será direcionado para a tela NossaTela
-        var i = Intent(this, NossaTela::class.java)
-        startActivity(i)
-
-
+        Handler(Looper.getMainLooper()).postDelayed({
+            // será direcionado para a tela NossaTela
+            var i = Intent(this, NossaTela::class.java)
+            startActivity(i)
+            finish()
+        }, 5000)
     }
 }
